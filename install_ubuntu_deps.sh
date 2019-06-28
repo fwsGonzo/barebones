eb2a894bbc7dbdd873b504e4aa2c3c5571e7f36c
Alf-André Walla <fwsgonzo@hotmail.com>
2019-06-28 22:35:00 +0200

    Several improvements to cmake build scripts

--- install_ubuntu_deps.sh
+++ install_ubuntu_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git submodule update --init --recursive
-sudo apt install build-essential gcc-multilib nasm qemu xorriso
+sudo apt install build-essential gcc-multilib nasm qemu xorriso cmake cmake-curses-gui
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 GRAPHICS=-nographic
+BUILD_DIR=build
 
 for i in "$@"
 do
@@ -17,20 +18,24 @@ case $i in
     OPTION="sanitize"
     shift # past argument with no value
     ;;
+	--clean)
+    rm -rf $BUILD_DIR/
+    exit 0
+    ;;
     *)
           # unknown option
+		  echo "--kvm, --vga, --sanitize, --clean"
     ;;
 esac
 done
 
-mkdir -p build
-pushd build
+mkdir -p $BUILD_DIR
+pushd $BUILD_DIR
 cmake ..
 make -j4 $OPTION
-BINARY=build/`cat binary.txt`
+BINARY=$BUILD_DIR/`cat binary.txt`
 popd
 
 # NOTE: if building with -march=native, make sure to enable KVM,
 # as emulated qemu only supports up to SSE3 instructions
 qemu-system-x86_64 $KVM -kernel tools/chainloader -initrd $BINARY $GRAPHICS
-#qemu-system-x86_64 $KVM -kernel tools/chainloader -initrd mykernel $GRAPHICS
