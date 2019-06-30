925516dd3e103988b54560d6d24dd63e52b50a42
Alf-André Walla <fwsgonzo@hotmail.com>
2019-07-01 00:02:20 +0200

    Add header for loading shared libraries

--- machines/shared/main.cpp
+++ machines/shared/main.cpp
@@ -3,32 +3,29 @@
 #include <cassert>
 #include <cstring>
 #include <cstdlib>
-#include <kernel/elf.hpp>
 
 extern char _binary_mylib_start;
 extern char _binary_mylib_end;
-
+#include <kernel/dylib.hpp>
 #include "library/library.hpp"
-static jumptable table;
 
 void kernel_main(const uint32_t, const uint32_t)
 {
+	// load address is the start of the mylib blob
+	auto* hdr = Dylib::load(&_binary_mylib_start);
+	assert(hdr != nullptr);
+
+	// translate init function name into symbol and create callable function
+	auto init = Dylib::resolve_function<init_function_t>(hdr, "init");
+	assert(init != nullptr);
+
 	// setup call table
+	static jumptable table;
 	table.kprintf = kprintf;
 	table.malloc = malloc;
 	table.free = free;
 
-	auto* hdr = (Elf64_Ehdr*) &_binary_mylib_start;
-	// validate shared library ELF header
-	assert(Elf::validate(hdr));
-	// resolve symbolic references from PLT and GOT
-	Elf::perform_relocations(hdr);
-	// translate init function name into symbol
-	const auto* sym = Elf::resolve_name(hdr, "init");
-	assert(sym != nullptr);
-	// create callable init function
-	auto library_init = (init_function_t) &((char*) hdr)[sym->st_value];
 	// call into library function
-	int result = library_init(&table);
+	int result = init(&table);
 	kprintf("init call result: %d\n", result);
 }
---
+++ src/kernel/dylib.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <kernel/elf.hpp>
+
+struct Dylib
+{
+	static Elf64_Ehdr* load(const void* address);
+
+	template <typename T>
+	static T resolve_function(Elf64_Ehdr* hdr, const char* name);
+};
+
+
+inline Elf64_Ehdr* Dylib::load(const void* address)
+{
+	auto* hdr = (Elf64_Ehdr*) address;
+	// validate shared library ELF header
+	assert(Elf::validate(hdr));
+	// resolve symbolic references from PLT and GOT
+	Elf::perform_relocations(hdr);
+	return hdr;
+}
+template <typename T>
+inline T Dylib::resolve_function(Elf64_Ehdr* hdr, const char* name)
+{
+	const auto* sym = Elf::resolve_name(hdr, name);
+	if (sym == nullptr) return T();
+	return (T) &((char*) hdr)[sym->st_value];
+}
