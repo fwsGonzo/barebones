b614b726479dbea47fab655bc80b5e5f2708812a
Alf-André Walla <fwsgonzo@hotmail.com>
2019-06-30 19:08:34 +0200

    Resolve symbolic references in shared library

--- machines/shared/library/library.cpp
+++ machines/shared/library/library.cpp
@@ -1,10 +1,24 @@
 #include "library.hpp"
+static int test_value = 666;
+
+struct Test {
+	int a;
+};
+Test test;
+
+extern "C"
+__attribute__((noinline))
+void test_function(jumptable* table)
+{
+	table->kprintf("Hello from test()! jumptable = %p\n", table);
+}
 
 extern "C"
 int init(jumptable* t)
 {
 	jumptable* table = t;
 	table->kprintf("Hello world from a shared library! table = %p\n", table);
+	table->kprintf("test_value == %d\n", test_value);
 
 	char* data = (char*) table->malloc(4096);
 	table->kprintf("malloc() returned %p\n", data);
@@ -14,5 +28,7 @@ int init(jumptable* t)
 	}
 	table->free(data);
 
+	test.a = 55;
+	test_function(t);
 	return 0;
 }
--- machines/shared/main.cpp
+++ machines/shared/main.cpp
@@ -9,6 +9,8 @@ extern char _binary_mylib_start;
 extern char _binary_mylib_end;
 static inline void validate_elf(Elf64_Ehdr* hdr);
 static const Elf64_Sym* resolve_name(Elf64_Ehdr* hdr, const char* name);
+static const Elf64_Shdr* section_by_name(Elf64_Ehdr* hdr, const char* name);
+static void elf_perform_relocations(Elf64_Ehdr* hdr);
 
 #include "library/library.hpp"
 static jumptable table;
@@ -23,18 +25,20 @@ void kernel_main(const uint32_t, const uint32_t)
 	auto* hdr = (Elf64_Ehdr*) &_binary_mylib_start;
 	// validate shared library ELF header
 	validate_elf(hdr);
-	// translate function name into symbol
+	// resolve symbolic references from PLT and GOT
+	elf_perform_relocations(hdr);
+	// translate init function name into symbol
 	const auto* sym = resolve_name(hdr, "init");
 	assert(sym != nullptr);
-
+	// create callable init function
+	auto library_init = (init_function_t) &((char*) hdr)[sym->st_value];
 	// call into library function
-	const char* address = (char*) hdr + sym->st_value;
-	int result = ((init_function_t) address)(&table);
+	int result = library_init(&table);
 	kprintf("init call result: %d\n", result);
 }
 
 template <typename T>
-inline const T* elf_offset(Elf64_Ehdr* hdr, intptr_t ofs) {
+inline T* elf_offset(Elf64_Ehdr* hdr, intptr_t ofs) {
   return (T*) &((char*) hdr)[ofs];
 }
 const Elf64_Sym* resolve_name(Elf64_Ehdr* hdr, const char* name)
@@ -71,6 +75,66 @@ const Elf64_Sym* resolve_name(Elf64_Ehdr* hdr, const char* name)
 	}
 
 	return nullptr;
+}
+
+const Elf64_Shdr* section_by_name(Elf64_Ehdr* hdr, const char* name)
+{
+	const auto* shdr = elf_offset<Elf64_Shdr> (hdr, hdr->e_shoff);
+	const auto& shstrtab = shdr[hdr->e_shnum-1];
+	const char* strings = elf_offset<char>(hdr, shstrtab.sh_offset);
+
+	for (auto i = 0; i < hdr->e_shnum; i++)
+	{
+		const char* shname = &strings[shdr[i].sh_name];
+		if (strcmp(shname, name) == 0) {
+			return &shdr[i];
+		}
+	}
+	return nullptr;
+}
+
+static const Elf64_Sym*
+resolve_dyn_idx(Elf64_Ehdr* hdr, uint32_t symidx)
+{
+	const auto* shdr = section_by_name(hdr, ".dynsym");
+	assert(symidx < shdr->sh_size / sizeof(Elf64_Sym));
+	auto* symtab = elf_offset<Elf64_Sym>(hdr, shdr->sh_offset);
+	return &symtab[symidx];
+}
+
+static void elf_print_sym(const Elf64_Sym* sym)
+{
+	kprintf("-> Sym is at %p with size %zu, type %u name %u\n",
+			(void*) sym->st_value, sym->st_size,
+			ELF64_ST_TYPE(sym->st_info), sym->st_name);
+}
+
+static void elf_relocate_section(Elf64_Ehdr* hdr, const char* section_name)
+{
+	const auto* rela = section_by_name(hdr, section_name);
+	const size_t rela_ents = rela->sh_size / sizeof(Elf64_Rela);
+	auto* rela_addr = elf_offset<Elf64_Rela>(hdr, rela->sh_offset);
+	for (size_t i = 0; i < rela_ents; i++) {
+		const uint32_t symidx = ELF64_R_SYM(rela_addr[i].r_info);
+		auto* sym = resolve_dyn_idx(hdr, symidx);
+
+		const uint8_t type = ELF64_ST_TYPE(sym->st_info);
+		if (type == STT_FUNC || type == STT_OBJECT)
+		{
+			uintptr_t entry = (uintptr_t) hdr + rela_addr[i].r_offset;
+			uintptr_t final = (uintptr_t) hdr + sym->st_value;
+			//kprintf("Relocating rela %zu with sym idx %u where %p -> %p\n",
+			//		i, symidx, (void*) entry, (void*) final);
+			//elf_print_sym(sym);
+			*(char**) entry = (char*) final;
+		}
+	}
+}
+
+void elf_perform_relocations(Elf64_Ehdr* hdr)
+{
+	elf_relocate_section(hdr, ".rela.plt");
+	elf_relocate_section(hdr, ".rela.dyn");
 }
 
 void validate_elf(Elf64_Ehdr* hdr) {
